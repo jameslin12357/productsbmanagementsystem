b891eda049bccf626de80047bec39e1c7d77b2fd
jameslin12357 <jameslin12357@qq.com>
2019-10-29 14:16:44 +0800

    cm1

---
+++ src/main/java/com/example/productsbmanagementsystem/ProductsbmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.productsbmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsbmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsbmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productsbmanagementsystem/ProductsbmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsbmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsbmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
